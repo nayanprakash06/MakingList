d3a649cd7694bceecc8145c5ec493a6f0ea27199
dishawasha <disha.somendra@gmail.com>
2025-03-20 11:33:01 +0530

    Update neobasic.py

--- neobasic.py
+++ neobasic.py
@@ -6,7 +6,7 @@ import neopixel
 
 np = neopixel.NeoPixel(Pin(4),16)
 
-np[0] = (255,0,0)
-np[12] = (0,0,255)
-np[10] = (0,255,0)
+np[0] = (240,0,0)
+np[12] = (0,226,255)
+np[10] = (218,255,173)
 np.write()
